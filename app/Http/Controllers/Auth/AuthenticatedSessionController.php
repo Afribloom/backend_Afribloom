ae498e019144009c2a3740012813109325c0edec
MOUCTAR LAWAN <lawan.mouctar@ensi.ma>
2025-03-11 17:02:37 +0100

    systeme d'authentification mise en place avec breeze API Register et login fonctionnt

---
+++ app/Http/Controllers/Auth/AuthenticatedSessionController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Auth\LoginRequest;
+use Illuminate\Http\Request;
+use App\Models\User;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AuthenticatedSessionController extends Controller
+{
+    /**
+     * Handle an incoming authentication request.
+     */
+    public function store(LoginRequest $request): JsonResponse
+    {
+        $request->authenticate();
+
+        $user = User::where('email', $request->email)->first();
+
+        $data = [
+            'token' => $user->createToken("token for user " . $user->email)->plainTextToken,
+            'user' => [
+                'name' => $user->name,
+                'email' => $user->email
+            ]
+        ];
+
+        return response()->json($data, 201);
+    }
+
+    /**
+     * Destroy an authenticated session.
+     */
+    public function destroy(Request $request): JsonResponse
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json(['message' => 'Logged out successfully'], 200);
+    }
+}
